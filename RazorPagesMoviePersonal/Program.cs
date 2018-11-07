9b1dbfa7ff4d47563a978f2b0d387ad1f5ef9cdf
Folivi Joshua Oluwaseun <joshua.folivi@gmail.com>
2018-11-07 12:14:43 +0100

    adding class SeedData

--- RazorPagesMoviePersonal/Program.cs
+++ RazorPagesMoviePersonal/Program.cs
@@ -5,8 +5,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using RazorPagesMoviePersonal.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMoviePersonal
 {
@@ -14,7 +18,26 @@ namespace RazorPagesMoviePersonal
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+	        {
+                var services = scope.ServiceProvider;
+
+                try 
+	            {	        
+		            var context = services.GetRequiredService<RazorPagesMoviePersonalContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);
+	            }
+	            catch (Exception ex)
+	            {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+		            logger.LogError(ex, "An Error Occured seeding the DB.");
+	            }
+	        }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
